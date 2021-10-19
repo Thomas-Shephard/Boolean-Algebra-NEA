94482943b160d103b52e3cf259c2f6ebc9da1f73
Thomas Shephard <thomas@thomas-shephard.com>
2021-10-19 21:41:14 +0100

    Commented ParserException

--- BooleanAlgebra/Parser/ParserException.cs
+++ BooleanAlgebra/Parser/ParserException.cs
@@ -2,12 +2,24 @@
 using BooleanAlgebra.Lexer.Lexemes;
 
 namespace BooleanAlgebra.Parser {
+    /// <summary>
+    /// Holds the position of an exception, thrown by the parser when it experiences a syntax error
+    /// </summary>
     public class ParserException : Exception {
+        /// <summary>
+        /// The position of the error within the input text.
+        /// </summary>
         public LexemePosition LexemePosition { get; }
         
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParserException"/> class with a <paramref name="lexemePosition"/> and <paramref name="message"/>.
+        /// </summary>
+        /// <param name="lexemePosition">The position of the error within the input text.</param>
+        /// <param name="message">The message that describes the error.</param>
+        /// <exception cref="ArgumentNullException">Thrown when either <paramref name="lexemePosition"/> or <paramref name="message"/> is null.</exception>
         public ParserException(LexemePosition lexemePosition, string message) : base (message) {
-            if (message is null) throw new ArgumentNullException(nameof(message));
-            LexemePosition = lexemePosition ?? throw new ArgumentNullException(nameof(lexemePosition));
+            if (message is null) throw new ArgumentNullException(nameof(message));                      //Ensure that message is not null
+            LexemePosition = lexemePosition ?? throw new ArgumentNullException(nameof(lexemePosition)); //Ensure that lexemePosition is not null
         }
     }
 }
