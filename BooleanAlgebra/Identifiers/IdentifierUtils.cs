1abffa60e7954fc57ac245c9808a9886aed9abcc
Thomas Shephard <thomas@thomas-shephard.com>
2021-10-23 20:36:20 +0100

    Fixe pattern matching in IdentifierUtils

--- BooleanAlgebra/Identifiers/IdentifierUtils.cs
+++ BooleanAlgebra/Identifiers/IdentifierUtils.cs
@@ -3,8 +3,8 @@ public static class IdentifierUtils {
     private static IEnumerable<Identifier> GetIdentifiers() {
         return new[] {
             new Identifier(IdentifierType.BINARY_OPERATOR, 1, "OR", "^((?i)OR|\\|)$", false),
-            new Identifier(IdentifierType.BINARY_OPERATOR, 2, "AND", "^((?i)AND|\\|)$", false),
-            new Identifier(IdentifierType.UNARY_OPERATOR, 3, "NOT", "^((?i)NOT|\\|)$", false),
+            new Identifier(IdentifierType.BINARY_OPERATOR, 2, "AND", "^((?i)AND|\\&)$", false),
+            new Identifier(IdentifierType.UNARY_OPERATOR, 3, "NOT", "^((?i)NOT|\\!)$", false),
             new Identifier(IdentifierType.GROUPING_OPERATOR_START, 4, "LEFT_PARENTHESIS", "^\\($", false),
             new Identifier(IdentifierType.GROUPING_OPERATOR_END, 4, "RIGHT_PARENTHESIS", "^\\)$", false),
             new Identifier(IdentifierType.OPERAND, 5, "VARIABLE", "^[A-Za-z]+$", true),
