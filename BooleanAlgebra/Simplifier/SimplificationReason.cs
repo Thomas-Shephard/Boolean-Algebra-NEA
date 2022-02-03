5f4f7558e36c6f46431617bb98dce21f270152be
Thomas Shephard <thomas@thomas-shephard.com>
2022-02-03 13:59:26 +0000

    Ay

--- BooleanAlgebra/Simplifier/SimplificationReason.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BooleanAlgebra.Simplifier;
-/// <summary>
-/// Holds information about a simplification and the reason why it was applied.
-/// </summary>
-/// <param name="SimplifiedSyntaxItem">The simplified form of a syntax item.</param>
-/// <param name="Reason">The reason by which a simplification can take place.</param>
-public record SimplificationReason(ISyntaxItem SimplifiedSyntaxItem, string Reason);
---
+++ BooleanAlgebra/Simplifier/SimplificationRulePair.cs
@@ -0,0 +1,7 @@
+﻿namespace BooleanAlgebra.Simplifier;
+/// <summary>
+/// Holds information about a single simplification of a boolean expression and the rule that was used to simplify it.
+/// </summary>
+/// <param name="SimplifiedSyntaxItem">A simplified form of the original boolean expression.</param>
+/// <param name="Rule">The rule that what used to allow the simplification to take place.</param>
+public record SimplificationRulePair(ISyntaxItem SimplifiedSyntaxItem, string Rule);
