d6cf77b5c4b18bc24cbbcde602c42a710fb01923
Thomas Shephard <thomas@thomas-shephard.com>
2022-02-03 13:58:34 +0000

    Noice

--- BooleanAlgebra/Simplifier/Simplifier.cs
+++ BooleanAlgebra/Simplifier/Simplifier.cs
@@ -1,34 +1,39 @@
 ﻿namespace BooleanAlgebra.Simplifier;
 /// <summary>
-/// 
+/// Provides a method to simplify a given syntax item into a simplified form and produce the respective simplification steps.
 /// </summary>
 public class Simplifier {
     /// <summary>
-    /// 
+    /// The message that is associated with the original syntax item in the simplification process.
+    /// This is the string that will describe the initial boolean expression.
     /// </summary>
     private const string StartMessage = "Initial boolean expression";
     /// <summary>
-    /// 
+    /// The original syntax item that is being simplified.
     /// </summary>
-    private ISyntaxItem StartSyntaxItem { get; }
+    private ISyntaxItem OriginalSyntaxItem { get; }
 
     /// <summary>
-    /// 
+    /// Initialises a new simplifier that will be able to simplify the given syntax item and produce the respective simplification steps.
     /// </summary>
-    /// <param name="startSyntaxItem"></param>
-    /// <exception cref="ArgumentNullException"></exception>
-    public Simplifier(ISyntaxItem startSyntaxItem) {
-        StartSyntaxItem = startSyntaxItem ?? throw new ArgumentNullException(nameof(startSyntaxItem));
+    /// <param name="originalSyntaxItem">The original syntax item that will be simplified.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="originalSyntaxItem"/> is null.</exception>
+    public Simplifier(ISyntaxItem originalSyntaxItem) {
+        OriginalSyntaxItem = originalSyntaxItem ?? throw new ArgumentNullException(nameof(originalSyntaxItem));
     }
 
     /// <summary>
-    /// 
+    /// Produces a list of steps that will simplify the original syntax item to a simplified form of itself.
     /// </summary>
-    /// <returns></returns>
-    public List<SimplificationReason> Simplify() {
-        SimplificationItem simplifierTree = new(StartSyntaxItem, StartMessage);
+    /// <returns>A list of steps that will simplify the original syntax item to a simplified form of itself.</returns>
+    public List<SimplificationRulePair> Simplify() {
+        //Create an apex node from the original syntax item so that it can be simplified.
+        //Use the start message as the message for the apex node.
+        SimplificationItem simplifierTree = new(OriginalSyntaxItem, StartMessage);
         simplifierTree.Simplify();
-        
-        return SimplificationItem.GetSmallestCostSyntaxItem(simplifierTree).GetSimplifiedSyntaxTree();
+        //Create a new list of steps to store the process by which the original syntax item was simplified.
+        List<SimplificationRulePair> simplificationSteps = new();
+        SimplificationItem.GetSmallestCostSyntaxItemWithSmallestDepth(simplifierTree).GetSimplificationSteps(simplificationSteps);
+        return simplificationSteps;
     }
 }
